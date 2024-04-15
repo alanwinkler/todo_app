a3203412f293a7ad9c68d9cac942f1cf4a21b453
Alan Winkler <alanquimeywinkler@gmail.com>
2024-04-15 19:37:25 -0300

    crud terminado

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import  CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
@@ -11,3 +14,19 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = "task"
+
+class TaskCreate(CreateView):
+    model = Task
+    # fields = [ 'title', 'description']
+    fields = '__all__'
+    success_url = reverse_lazy('task')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('task')
